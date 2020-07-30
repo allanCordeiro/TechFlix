493a1a50c2b2994c919750f6a53cb3d6dbee48e6
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2020-07-30 16:03:53 -0300

    Inicio do projeto e conteudo da primeira aula

---
+++ src/components/ButtonLink/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ButtonLink(props) {
+    // props => { className: "", href: "", Child: "" }
+    return (
+        <a className={props.className} href={props.href}>{props.children}</a>
+    );
+}
+
+export default ButtonLink;
